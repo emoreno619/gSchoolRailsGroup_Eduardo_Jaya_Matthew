b9f013d0957c484734fe1d8cab2bf346339950ef
JayaDarby <jayadarby@gmail.com>
2015-07-27 16:52:52 -0700

    added index, new, and create routes/views

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,14 +1,27 @@
 class FoodsController < ApplicationController
   def index
+  	@foods = Food.all
+  	if params[:user_id]
+  		@user_food = Food.where(user_id: params[:user_id])
+  	end
   end
 
   def new
+  	if params[:user_id]
+  	   @food = Food.new
+  	end
   end
 
   def create
+  	if params[:user_id]
+  		@food = Food.create food_params
+  	end
   end
 
   def show
+  	if params[:user_id]
+  		@food = Food.where(user_id: params[:user_id]).where(id: params[:id])
+  	end
   end
 
   def edit
@@ -19,5 +32,10 @@ class FoodsController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+  	def food_params
+  		params.require(:food).permit(:name, :gluten_free, :vegan, :image_url)
+    end
+
 end
