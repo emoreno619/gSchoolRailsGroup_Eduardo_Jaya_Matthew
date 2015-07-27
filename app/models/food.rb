2cc47810a01d64e2c4bb5eed3663cf3365f8407b
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-27 16:47:30 -0700

    includes some fixes for many to many

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,9 @@ class Food < ActiveRecord::Base
 
 	belongs_to :user
 
+	has_many :foodlocations, dependent: :destroy
+	has_many :locations, through: :foodlocations
+
 	validates :name, presence: true
 	validates :gluten_free, :inclusion => {:in => [true, false]}
 	validates :vegan, :inclusion => {:in => [true, false]}
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
 	has_many :foods
 	has_many :locations
 
+	has_many :locationusers, dependent: :destroy
 end
