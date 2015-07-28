acd6be467567eeb2f34a27d378959a4ad6f847c2
JayaDarby <jayadarby@gmail.com>
2015-07-27 20:40:46 -0700

    added edit, update, and destroy controllermethod/views

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -15,22 +15,38 @@ class FoodsController < ApplicationController
   def create
   	if params[:user_id]
   		@food = Food.create food_params
+  		if @food.save
+  			redirect_to user_foods_path(:user_id), flash:{success:'Food successfully created!'}
+  		else
+  			render :new
+  		end
   	end
   end
 
   def show
   	if params[:user_id]
-  		@food = Food.where(user_id: params[:user_id]).where(id: params[:id])
+  		@food = Food.where(id: params[:id])[0]
   	end
   end
 
   def edit
+  	if params[:user_id]
+  		@food = Food.where(id: params[:id])[0]
+  	end
   end
 
   def update
+  	if params[:user_id]
+  	  	if @food.update
+  			redirect_to user_foods_path(:user_id), flash:{success:'Food successfully updated!'}
+  		else
+  			render :edit
+  		end
+  	end
   end
 
   def destroy
+
   end
 
   private
