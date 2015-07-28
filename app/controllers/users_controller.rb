869a29cc59e87a7e8d7f897b0f312c04282a659f
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-27 21:36:27 -0700

    fixes user create route and updates index view with links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,11 @@ class UsersController < ApplicationController
 
   def new
   	@user = User.new
+
   end
 
   def create
+  	@user = User.new(user_params)
 	if @user.save
 		redirect_to users_path
 	else
@@ -27,4 +29,9 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:email,:password)
+  	end
 end
