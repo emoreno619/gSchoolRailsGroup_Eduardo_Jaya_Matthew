04d5b47bf286a41ec894206d87d5c97b6377a44c
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-27 22:07:50 -0700

    fixes users destroy route and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,13 +21,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+  	@user = User.find_by_id(params[:id])
   end
 
   def update
-  	
+  	@user = User.new
+  	@user.update_attributes(user_params)
+  	if @user.save
+  		redirect_to users_path
+  	else
+  		render :edit
+  	end
   end
 
   def destroy
+  	@user = User.find_by_id(params[:id])
+  	@user.destroy
+  	redirect_to users_path
   end
 
   private
