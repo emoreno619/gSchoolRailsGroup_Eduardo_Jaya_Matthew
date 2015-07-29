eea735b561e5b967a451b46e13bbc10008211669
JayaDarby <jayadarby@gmail.com>
2015-07-29 12:21:33 -0700

    adding auth

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'typhoeus'
 gem 'sidekiq'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
+  root 'locations#index'
 	post '/locations/search', to: 'locations#search'
   
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/login', to: 'users#login'
   post '/login', to: 'users#attempt_login'
-  get '/signup', to: 'users#create'
-  post '/signup', to: 'users#index'
   delete '/logout', to: 'users#logout'
 
 
