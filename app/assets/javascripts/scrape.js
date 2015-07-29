fc66c705c96cd1b4fa3606322c5b0b3d8278a02e
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-29 14:45:53 -0700

    pushes scrape data from server to client in ajax response

--- app/assets/javascripts/scrape.js
+++ app/assets/javascripts/scrape.js
@@ -6,14 +6,28 @@ $(function(){
 		searchQuery = {}
 		searchQuery.keyword = $('#keyword').val()
 		searchQuery.location = $('#location').val()
+		
 		$.ajax({
 				  type: 'POST',
 				  url: '/locations/search',
 				  data: searchQuery,
 				  dataType: 'json'
 				}).done(function(datas) {
-
+					console.log(datas)
 				})
 
 	})
+	// $.ajax({
+	// 		  type: 'POST',
+	// 		  url: '/locations/search',
+	// 		  dataType: 'json'
+	// 		}).done(function(datas) {
+
+	// 		})
+
+	$('#gluten-event').change(function(){
+		console.log("TOGGLED ON")
+
+	})
+
 });
