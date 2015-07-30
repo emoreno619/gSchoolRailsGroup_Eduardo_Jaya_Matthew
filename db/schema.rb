0050560d8440e0a2963f2dd39305fda9fd32bcba
JayaDarby <jayadarby@gmail.com>
2015-07-30 15:00:38 -0700

    adding migrations for lat and long

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728184824) do
+ActiveRecord::Schema.define(version: 20150730215840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20150728184824) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.boolean  "is_public"
+    t.string   "latitude"
+    t.string   "longitude"
   end
 
   create_table "locationusers", force: :cascade do |t|
