a712ff3ef9f52e4c5c94beefd0734ad4c1bd1593
JayaDarby <jayadarby@gmail.com>
2015-07-30 15:30:23 -0700

    password reset in the making

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730215840) do
+ActiveRecord::Schema.define(version: 20150730222115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20150730215840) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.boolean  "is_admin"
+    t.string   "reset_token"
   end
 
   add_foreign_key "foodlocations", "foods"
